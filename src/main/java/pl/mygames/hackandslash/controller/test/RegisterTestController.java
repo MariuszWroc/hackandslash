2820419b52bfc6e4548b81f7009f1462743bd6e5
MCZARNY <MCZARNY@CE12773.corp.capgemini.com>
2015-11-24 15:06:01 +0100

    Poprawienie dao (brakujący parametr w zapytaniu). Poprawienie walidacji formularza do rejestrownania użytkownika.

--- src/main/java/pl/mygames/hackandslash/controller/test/RegisterTestController.java
+++ src/main/java/pl/mygames/hackandslash/controller/test/RegisterTestController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 

482742dc41f146edb6469ec8c40964bf0fa6d6ce
Jacek <jaca09001@gmail.com>
2015-12-16 23:50:15 +0100

    Dodano czyyszczenie formatek user i hero

--- src/main/webapp/resources/js/heroController.js
+++ src/main/webapp/resources/js/heroController.js
@@ -1,11 +1,16 @@
 module.controller('heroController', ['$scope', '$http', function($scope, $http){
-	
+    $scope.heroDetail = {};
+    
     $http.get(prefix + '/user/actualHero')
-	    .success(function(response){
-            console.log('Fetching hero success ', response);
-	        $scope.heroDetail = response;
-	    })
-	    .error(function(error){
-	        console.log("Error after getting hero. ", error);
-	    });
+        .success(function(response){
+        console.log('Fetching hero success ', response);
+            $scope.heroDetail = response;
+        })
+        .error(function(error){
+            console.log("Error after getting hero. ", error);
+        });
+            
+    $scope.reset = function(){
+        $scope.heroDetail = {};
+    };
 }]);
--- src/main/webapp/resources/js/userController.js
+++ src/main/webapp/resources/js/userController.js
@@ -1,5 +1,6 @@
 module.controller('userController', ['$scope', '$http', function($scope, $http){
-
+    
+    $scope.userDetail = {};
     $http.get(prefix + '/user/actualProfil')
         .success(function(res){
             console.log('is user logged? ',res);
@@ -8,5 +9,9 @@ module.controller('userController', ['$scope', '$http', function($scope, $http){
         .error(function(error){
             console.log("Error after getting user " + error);
         });
-
+        
+        $scope.reset = function(){
+            console.log('test');
+            $scope.userDetail = {};
+        };
 }]);
